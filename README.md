3dece413c112a4a4910780d2637da9b3b3db2ee0
Rogelio Lozano <rjlozano58@gmail.com>
2025-08-28 20:15:34 -0500

    Added url to github read me

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CS484 HW0
 
 Used Vite to create a React.js website, deployed with github pages.
+
+Visit the URL -> https://rjlozano58.github.io/CS484-HW0/
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base:"/cs484-hw0",
-=======
   base:"/CS484-HW0/",
->>>>>>> 74bc20a (added deployed url in url.txt)
 })
