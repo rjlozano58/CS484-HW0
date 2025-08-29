708103e3ba77cb2f88f17c9819583071c77845d5
Rogelio Lozano <rjlozano58@gmail.com>
2025-08-28 19:27:23 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>rloza4@uic.edu</h1>
+      <h2>CS 484 Homework 0</h2>
+      
+      
+    </>
+  )
+}
+
+export default App
